f4c816e4b732661296b88fe41442b0a4178b9b46
Tejas Falke <tejasdf.77@gmail.com>
2022-10-10 21:46:57 +0530

    let redelcare error and also iet variable update is alloweed

---
+++ 01.Variables/01.declare_variables/02.let/02.let_error.js
@@ -0,0 +1,7 @@
+//let can be updated 
+let greeting = "say Hi";
+greeting = "say Hello instead";
+
+//but not re-declared
+let greeting = "say Hi";
+let greeting = "say Hello instead"; // error: Identifier 'greeting' has already been declared
