eba930e38b12d295a8ab336d163d628a00441f6b
Tejas Falke <tejasdf.77@gmail.com>
2022-10-15 09:59:02 +0530

    Add Function Scope

---
+++ 01.Introduction_of_JavaScript/03.Variable Scope/04.FunctionScope.js
@@ -0,0 +1,14 @@
+// Ex. var
+function myFunction1(){
+    var carName = "volvo";//Function Scope
+}
+
+// Ex. let
+function myFunction2(){
+    let carName = "volvo";//Function Scope
+}
+
+// Ex. const
+function myFunction3(){
+    const carName = "volvo";//Function Scope
+}
